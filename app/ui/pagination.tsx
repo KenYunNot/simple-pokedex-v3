a7f57a8ffce92d7f92ea5dfe9031741b17703b16
Ken Yun <yunhaen@gmail.com>
2023-12-25 10:48:40 -0500

    Created pagination component

---
+++ app/ui/pagination.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+
+import { usePathname, useSearchParams } from 'next/navigation';
+
+export default function Pagination({ totalPages } : { totalPages: number}) {
+  // const pathname = usePathname();
+  // const searchParams = useSearchParams();
+  // const currentPage = Number(searchParams.get('page')) || 1;
+  
+  // function createPageURL(pageNumber: number | string) {
+  //   const params = new URLSearchParams(searchParams);
+  //   params.set('page', pageNumber.toString());
+  //   return `${pathname}?${params.toString()}`;
+  // }
+
+  return (
+    <div>
+      temp
+    </div>
+  )
+}
