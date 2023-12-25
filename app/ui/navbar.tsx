677b6d1217b1466c69fd73b1262052e42d2c31c1
Ken Yun <yunhaen@gmail.com>
2023-12-25 10:47:35 -0500

    Added Tailwind breakpoints to center the icon on smaller screen sizes (below 400px)

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="p-3 bg-red-500">
+    <nav className="flex justify-center sm:justify-start p-3 bg-red-500">
       <Link
         href='/'  
       >
         <img 
-          className="mx-3 w-96"
+          className="px-3 w-96"
           src="https://fontmeme.com/permalink/231224/e059f38d9511eda4f7e9afb0a94d2594.png" 
           alt="pokemon-font" 
         />
