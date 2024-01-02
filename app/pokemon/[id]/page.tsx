72a3ef56a5bcc20a449d231fe5b66d4628b842e5
Ken Yun <yunhaen@gmail.com>
2024-01-02 17:14:00 -0500

    Replace img tag with Image tag from next/image

--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Table from "@/app/ui/pokemon/table";
 import { notFound } from "next/navigation";
 import { fetchPokemonById } from "@/app/lib/data";
@@ -15,7 +16,10 @@ export default async function Pokemon({ params } : { params: { id: string }}) {
     <div className="m-3">
       <h1 className="pb-4 text-3xl text-center font-semibold border-b border-gray-300">{pokemon.species.full_name}</h1>
       <div>
-        <img src={pokemon.image_url} />
+        <Image 
+          src={pokemon.image_url} 
+          alt={`Image of ${pokemon.name}`}
+        />
         <Table header="Pokédex data" pokemon={pokemon} />
         <Table header="Training data" pokemon={pokemon} />
       </div>
