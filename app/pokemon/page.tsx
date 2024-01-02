84d38fa8debc6e6621086bd25d686ad4baed9237
Ken Yun <yunhaen@gmail.com>
2024-01-02 17:07:16 -0500

    Moved card elements to separate UI component

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Search from "@/app/ui/search";
 import Pagination from "@/app/ui/pagination";
+import PokemonCard from "@/app/ui/pokemon/pokemon-card";
 import { 
   fetchPokemon,
   countPokemonPages, 
 } from "@/app/lib/data";
-import { capitalize } from "@/app/lib/utils";
 
-export default async function Home({
+export default async function PokemonList({
   searchParams,
 }: {
   searchParams?: {
@@ -30,18 +30,8 @@ export default async function Home({
       ">
         {pokemon.map((p) => {
           return (
-            <Link href={`/pokemon/${p.pokemon_id}`} key={p.pokemon_id}>
-              <div className="relative flex flex-col justify-between items-center py-3 border-2 border-gray-300 rounded-lg hover:bg-gray-300" style={{height: '650px'}}>
-                <div className="flex justify-between w-full px-3">
-                  <div className="inline px-2 text-2xl text-white font-bold bg-red-500 rounded-xl">{p.pokemon_id}</div>
-                  <p className="inline text-2xl font-bold">{p.full_name}</p>
-                </div>
-                <img src={p.image_url} className="p-8" />
-                <div className="flex justify-center items-center w-full">
-                  <div className={`type-icon ${p.type1}`}>{p.type1.toUpperCase()}</div>
-                  {p.type2 && <div className={`type-icon ${p.type2}`}>{p.type2.toUpperCase()}</div>}
-                </div>
-              </div>
+            <Link href={`/pokemon/${p.id}`} key={p.id}>
+              <PokemonCard pokemon={p} />
             </Link>
           );
         })}
