476a1d8b8699146b852d30ef2d28258707e82d0b
Ken Yun <yunhaen@gmail.com>
2024-01-04 16:44:37 -0500

    Removed type-icon class and replaced with Tailwind classes

--- app/ui/pokemon/type-icon.tsx
+++ app/ui/pokemon/type-icon.tsx
@@ -1,6 +1,8 @@
 
-export default function TypeIcon({ type } : { type: string }) {
+export default function TypeIcon({ type, shortened } : { type: string, shortened?: boolean }) {
+
+
   return (
-    <div className={`type-icon ${type}`}>{type.toUpperCase()}</div>
+    <div className={`flex justify-center items-center w-20 h-8 mx-1 text-white text-xs font-bold border border-gray-400 rounded-md text-shadow ${type}`}>{type.toUpperCase()}</div>
   );
 }
