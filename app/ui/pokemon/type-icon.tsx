c40a9522a45d27baa04f1228e31bbd8f81884301
Ken Yun <yunhaen@gmail.com>
2024-01-06 15:34:33 -0500

    Renamed iconType variable to iconStyle for more clarity

--- app/ui/pokemon/type-icon.tsx
+++ app/ui/pokemon/type-icon.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 
 export default function TypeIcon({ type, shortened } : { type: string, shortened?: boolean }) {
-  const iconType = clsx({
+  const iconStyle = clsx({
     "w-20 h-8 mx-1 font-bold" : shortened === false || shortened === undefined,
     "w-8 h-8 font-semibold" : shortened === true,
   })
@@ -9,6 +9,6 @@ export default function TypeIcon({ type, shortened } : { type: string, shortened
   const iconText = shortened ? type.slice(0, 3).toUpperCase() : type.toUpperCase();
 
   return (
-    <div className={`flex justify-center items-center text-white text-xs border border-gray-400 rounded-md text-shadow ${iconType} ${type}`}>{iconText}</div>
+    <div className={`flex justify-center items-center text-white text-xs border border-gray-400 rounded text-shadow ${iconStyle} ${type}`}>{iconText}</div>
   );
 }
