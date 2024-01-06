0615bd41d1e28d3796078e3b5b8ad601cc1a7dae
Ken Yun <yunhaen@gmail.com>
2024-01-05 19:34:20 -0500

    Created fetchPokemonSpeciesById, fetchPokemonFullName, and fetchPokemonId functions

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -29,7 +29,7 @@ export async function fetchPokemon(
   }
 }
 
-/* Fetches Pokemon data by the given id */
+/* Fetches Pokemon data by the given id or name */
 export async function fetchPokemonById(id: number) {
   try {
     let pokemon = await prisma.pokemon.findFirst({
@@ -39,16 +39,77 @@ export async function fetchPokemonById(id: number) {
       include: {
         types: true,
         species: true,
-      },
+      }
     });
     return pokemon;
   } catch (error) {
     console.error("Database Error:", error);
-    throw new Error("Failed to fetch Pokemon data");
+    throw new Error(`Failed to fetch Pokemon with ID ${id}`);
   }
 }
 
-/* Counts the total number of pages for a given query */
+export async function fetchPokemonSpeciesById(id: number) {
+  try {
+    let species = await prisma.pokemonSpecies.findFirst({
+      where: {
+        id,
+      },
+      include: {
+        pokemon: {
+          include: {
+            species: true,
+            types: true,
+          }
+        }
+      },
+    });
+    return species;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error(`Failed to fetch Pokemon species with ID ${id}`);
+  }
+}
+
+export async function fetchPokemonFullName(id: number) {
+  try {
+    let species = await prisma.pokemonSpecies.findFirst({
+      where: {
+        id,
+      },
+      select: {
+        full_name: true
+      }
+    });
+    return species ? species.full_name : undefined;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error(`Failed to fetch Pokemon species with ID ${id}`);
+  }
+}
+
+/* Takes an identifier either in the form of a number id or a string name and returns the corresponding Pokemon's id */
+export async function fetchPokemonId(identifier: string) {
+  let id = Number(identifier);
+  // If the identifier is already a number, return it as the id
+  if (!Number.isNaN(id)) 
+    return id;
+  try {
+    let pokemon = await prisma.pokemonSpecies.findFirst({
+      where: {
+        name: identifier,
+      },
+    }); 
+    // If no Pokemon was found, return an invalid id
+    if (!pokemon) 
+      return -1;
+    return pokemon.id;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error(`Failed to fetch Pokemon ID for \'${identifier}\'`);
+  }
+}
+
+/* Counts the total number of pages (16 Pokemon per page) for a given query */
 export async function countPokemonPages(query: string) {
   try {
     let aggregations = await prisma.pokemon.aggregate({
@@ -59,12 +120,13 @@ export async function countPokemonPages(query: string) {
         name: {
           contains: query,
         },
+        is_default: true,
       },
     });
     let count = aggregations._count.name;
     return Math.ceil(count / ITEMS_PER_PAGE);
   } catch (error) {
     console.error("Database Error:", error);
-    throw new Error("Failed to count pages");
+    throw new Error("Failed to count Pokemon pages");
   }
 }
