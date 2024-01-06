f3107d122fc06f4235f2db30412754e5098e8d33
Ken Yun <yunhaen@gmail.com>
2024-01-06 15:34:04 -0500

    Added padding around around Pokemon image

--- app/ui/pokemon/pokemon-card.tsx
+++ app/ui/pokemon/pokemon-card.tsx
@@ -4,13 +4,15 @@ import { Pokemon } from "@/app/lib/definitions"
 
 export default async function PokemonCard({ pokemon } : { pokemon : Pokemon }) {
   return (
-    <div className="relative flex flex-col justify-between items-center py-3 border-2 border-gray-300 rounded-lg hover:bg-gray-300" style={{height: '650px'}}>
+    // <div className="relative flex flex-col justify-between items-center py-3 border-2 border-gray-300 rounded-lg hover:bg-gray-300" style={{height: '650px'}}>
+    <div className="relative flex flex-col justify-between items-center h-min py-3 border-2 border-gray-300 rounded-lg hover:bg-gray-300">
       <div className="flex justify-between w-full px-3">
         <div className="inline px-2 text-2xl text-white font-bold bg-red-500 rounded-xl">{pokemon.id}</div>
         <p className="inline text-2xl font-bold">{pokemon.species.full_name}</p>
       </div>
       <Image 
         src={pokemon.image_url}
+        className="p-4"
         width={475}
         height={475}
         alt={`Image of ${pokemon.name}`}
