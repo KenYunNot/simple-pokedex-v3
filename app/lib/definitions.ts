bc36f63cc07d46ea533d4580c0da7bc7e9e8ed03
Ken Yun <yunhaen@gmail.com>
2024-01-05 19:36:49 -0500

    Removed unnecessary type definitions and refactored existing ones

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -9,12 +9,23 @@ export type Pokemon = Prisma.PokemonGetPayload<{
 
 export type PokemonSpecies = Prisma.PokemonSpeciesGetPayload<{
   include: {
-    pokemon: true,
+    pokemon: {
+      include: {
+        species: true,
+        types: true,
+      }
+    }
   }
 }>;
 
 export type Type = Prisma.TypeGetPayload<{
   include: {
+    double_damage_from: true,
+    double_damage_to: true,
+    half_damage_from: true,
+    half_damage_to: true,
+    no_damage_from: true,
+    no_damage_to: true,
     pokemon: true,
   }
 }>;
