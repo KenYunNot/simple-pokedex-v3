66d32a52ea3f3a4425c056079e935c7d566f41e0
Ken Yun <yunhaen@gmail.com>
2024-01-06 15:35:40 -0500

    Renamed [idOrName] folder to match route name

---
+++ app/ui/pokemon/[identifier]/breeding.tsx
@@ -0,0 +1,32 @@
+import Section from "@/app/ui/pokemon/[identifier]/data-section";
+import { Pokemon } from "@/app/lib/definitions";
+
+export default function BreedingData({ pokemon } : { pokemon : Pokemon }) {
+  return (
+    <Section header="Breeding">
+      <table className="data-table">
+        <tbody>
+          <tr>
+            <th>Egg Groups</th>
+            <td>
+              {pokemon.species.egg_groups.map((group, index) => {
+                if (index === 0) {
+                  return group;
+                }
+                return ", " + group;
+              })}
+            </td>
+          </tr>
+          <tr>
+            <th>Gender</th>
+            <td><span id="male">{(1000 - (pokemon.species.gender_rate * 125)) / 10}% male</span>, <span id="female">{pokemon.species.gender_rate * 125 / 10}% female</span></td>
+          </tr>
+          <tr>
+            <th>Egg Cycles</th>
+            <td>{pokemon.species.egg_cycles} <span className="text-gray-500 text-xs">(about ~{pokemon.species.egg_cycles * 255} steps)</span></td>
+          </tr>
+        </tbody>
+      </table>
+    </Section>
+  )
+}
---
+++ app/ui/pokemon/[identifier]/type-defenses.tsx
@@ -0,0 +1,65 @@
+import Section from "@/app/ui/pokemon/[identifier]/data-section";
+import TypeIcon from "@/app/ui/pokemon/type-icon";
+import { Pokemon, TYPES } from "@/app/lib/definitions";
+import { generateTypeDefenses } from "@/app/lib/utils";
+import clsx from "clsx";
+
+export default async function TypeDefenses({ pokemon }: { pokemon: Pokemon }) {
+  const type_defenses = await generateTypeDefenses(pokemon.types);
+  const multiplier = clsx({});
+
+  return (
+    <Section header="Type defenses">
+      <p>The effectiveness of each type on <span className="italic">{pokemon.species.full_name}</span></p>
+      <div className="flex flex-col items-center">
+        <TypeTable start={0} end={TYPES.length/2} type_defenses={type_defenses} />
+        <TypeTable start={TYPES.length/2} type_defenses={type_defenses} />
+      </div>
+    </Section>
+  );
+}
+
+function TypeTable({
+  start,
+  end,
+  type_defenses,
+}: {
+  start: number;
+  end?: number;
+  type_defenses: {[key: string] : number};
+}) {
+  if (!end) end = TYPES.length;
+
+  return (
+    <div className="flex mt-4">
+      {TYPES.slice(start, end).map((type) => {
+        let element: string | number;
+        if (type_defenses[type] === 0.25) element = "\u00BC";
+        else if (type_defenses[type] === 0.5) element = "\u00BD";
+        else if (type_defenses[type] === 1) element = "";
+        else element = type_defenses[type];
+        let style = clsx(
+          'text-yellow-400',
+          {
+            "bg-gray-900" : type_defenses[type] === 0,
+            "bg-red-900" : type_defenses[type] === 0.25,
+            "bg-red-600" : type_defenses[type] === 0.5,
+            "bg-green-600" : type_defenses[type] === 2,
+            "bg-green-900" : type_defenses[type] === 4,
+          }
+        )
+        return (
+          <div className="flex flex-col" key={type}>
+            <TypeIcon type={type} shortened={true} />
+            <div
+              className={`flex justify-center items-center w-8 h-8 border ${style}`}
+              key={type}
+            >
+              {element}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
