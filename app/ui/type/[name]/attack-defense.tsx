061a6df3b036e7f89ac1fed6ab4d6774c00f006c
Ken Yun <yunhaen@gmail.com>
2024-01-08 14:04:46 -0500

    Created folder for /type UI components

---
+++ app/ui/type/[name]/attack-defense.tsx
@@ -0,0 +1,66 @@
+import Link from "next/navigation";
+import { Type } from "@/app/lib/definitions";
+import { Type as SimpleType } from "@prisma/client";
+import { capitalize } from "@/app/lib/utils";
+import TypeIcon from "@/app/ui/type-icon";
+
+export default function AttackDefense({ type } : { type: Type}) {
+  
+  function buildTypeIconsList(damage_relation: SimpleType[]) {
+    return (
+      <div className="flex m-3">
+        {damage_relation.map((relation) => {
+          return (
+            <div key={relation.name}>
+              <TypeIcon type={relation} link={true} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+  
+  return (
+    <div className="flex flex-col">
+      <h2 className="text-4xl font-bold">Attack <span className="text-gray-600 italic">pros & cons</span></h2>
+      {type.double_damage_to.length > 0 && (
+        <div>
+          <p><span className="italic">{capitalize(type.name)}</span> moves are super-effective against:</p>
+          {buildTypeIconsList(type.double_damage_to)}
+        </div>
+      )}
+      {type.half_damage_to.length > 0 && (
+        <div>
+          <p><span className="italic">{capitalize(type.name)}</span> moves are not very effective against:</p>
+        {buildTypeIconsList(type.half_damage_to)}
+        </div>
+      )}
+      {type.no_damage_to.length > 0 && (
+        <div>
+          <p><span className="italic">{capitalize(type.name)}</span> moves have no effect on:</p>
+          {buildTypeIconsList(type.no_damage_to)}
+        </div>
+      )}
+      
+      <h2 className="text-4xl font-bold">Defense <span className="text-gray-600 italic">pros & cons</span></h2>
+      {type.no_damage_from.length > 0 && (
+        <div>
+          <p>These types have no effect against <span className="italic">{capitalize(type.name)}</span> Pokémon:</p>
+          {buildTypeIconsList(type.no_damage_from)}
+        </div>
+      )}
+      {type.half_damage_from.length > 0 && (
+        <div>
+          <p>These types are not very effective against <span className="italic">{capitalize(type.name)}</span> Pokémon:</p>
+          {buildTypeIconsList(type.half_damage_from)}
+        </div>
+      )}
+      {type.double_damage_from.length > 0 && (
+        <div>
+          <p>These types are super effective against <span className="italic">{capitalize(type.name)}</span> Pokémon:</p>
+          {buildTypeIconsList(type.double_damage_from)}
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ app/ui/type/[name]/dual-type.tsx
@@ -0,0 +1,5 @@
+import { Type } from "@/app/lib/definitions";
+
+export default function DualType({ type } : { type: Type}) {
+  
+}
