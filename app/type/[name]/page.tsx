e14cd6bf0b521a4a48ef38d49d9cfbef782e860b
Ken Yun <yunhaen@gmail.com>
2024-01-09 13:07:25 -0500

    Created basic landing page for the route

--- app/type/[name]/page.tsx
+++ app/type/[name]/page.tsx
@@ -1,6 +1,8 @@
 import { notFound } from "next/navigation";
-import AttackDefense from "@/app/ui/type/[name]/attack-defense";
 import { fetchTypeByName } from "@/app/lib/data";
+import { capitalize } from "@/app/lib/utils";
+import AttackDefense from "@/app/ui/type/[name]/attack-defense";
+import DualTypeAttack from "@/app/ui/type/[name]/dual-type";
 
 export default async function Type({ params } : { params: { name: string }}) {
   const name = params.name;
@@ -13,7 +15,9 @@ export default async function Type({ params } : { params: { name: string }}) {
 
   return (
     <div>
+      <h1 className="page-header">{capitalize(type.name)} <span className="text-gray-500">(type)</span></h1>
       <AttackDefense type={type} />
+      <DualTypeAttack type={type} />
     </div>
   )
 }
