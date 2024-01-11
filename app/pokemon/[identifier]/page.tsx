9d70dd9d11a9bec9a09f0330eea29a0a488fd392
Ken Yun <yunhaen@gmail.com>
2024-01-11 13:52:00 -0500

    Updated href prop and text content of Link tags to use the new fields in PokemonSpecies

--- app/pokemon/[identifier]/page.tsx
+++ app/pokemon/[identifier]/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import PokemonTabs from "@/app/ui/pokemon/[identifier]/pokemon-tabs";
 import PokemonPanels from "@/app/ui/pokemon/[identifier]/pokemon-panels";
-import { fetchPokemonSpeciesById, fetchPokemonId, fetchPokemonNames } from "@/app/lib/data";
+import { fetchPokemonSpeciesById, fetchPokemonId, fetchPokemonSpeciesNames } from "@/app/lib/data";
 import { 
   ChevronLeftIcon, 
   ChevronRightIcon, 
@@ -18,23 +18,20 @@ export default async function Pokemon({ params }: { params: { identifier: string
     notFound();
   }
 
-  const left = await fetchPokemonNames(id-1);
-  const right = await fetchPokemonNames(id+1);
-
   const linkStyle = "flex px-1 items-center text-blue-700 hover:bg-gray-300"
 
   return (
     <div className="m-4">
       <nav className="w-full table clear-both my-1 px-2">
-        {left && (
-          <Link href={`/pokemon/${left.name}`} className={`${linkStyle} float-left rounded-l-lg`}>
+        {species.left_full_name && (
+          <Link href={`/pokemon/${species.left_name}`} className={`${linkStyle} float-left rounded-l-lg`}>
             <ChevronLeftIcon className="inline w-4 h-4" />
-            #{String(id-1).padStart(4, '0')} {left.full_name}
+            #{String(id-1).padStart(4, '0')} {species.left_full_name}
           </Link>
         )}
-        {right && (
-          <Link href={`/pokemon/${right.name}`} className={`${linkStyle} float-right rounded-r-lg`}>
-            #{String(id+1).padStart(4, '0')} {right.full_name}
+        {species.right_full_name && (
+          <Link href={`/pokemon/${species.right_name}`} className={`${linkStyle} float-right rounded-r-lg`}>
+            #{String(id+1).padStart(4, '0')} {species.right_full_name}
             <ChevronRightIcon className="inline w-4 h-4" />
           </Link>
         )}
