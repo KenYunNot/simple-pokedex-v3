58a008cdaf6c7a34c0595f09a98df9bca2408536
Ken Yun <yunhaen@gmail.com>
2024-01-15 13:21:05 -0500

    Removed unnecessary Tailwind classnames from main div

--- app/ui/pokemon/pokemon-card.tsx
+++ app/ui/pokemon/pokemon-card.tsx
@@ -4,7 +4,7 @@ import { Pokemon } from "@/app/lib/definitions"
 
 export default async function PokemonCard({ pokemon } : { pokemon : Pokemon }) {
   return (
-    <div className="relative flex flex-col justify-between items-center h-min py-3 border-2 border-gray-300 rounded-lg hover:bg-gray-300">
+    <div className="flex flex-col items-center h-min py-3 border-2 border-gray-300 rounded-lg hover:bg-gray-300">
       <div className="flex justify-between w-full px-3">
         <div className="inline px-2 text-2xl text-white font-bold bg-red-500 rounded-xl">{pokemon.id}</div>
         <p className="inline text-2xl font-bold">{pokemon.species.full_name}</p>
