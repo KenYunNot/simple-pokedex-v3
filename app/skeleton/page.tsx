1d25e460bf9ebc34e811899059bf9b14549a1bba
Ken Yun <yunhaen@gmail.com>
2024-01-18 16:55:56 -0500

    Dummy commit

--- app/skeleton/page.tsx
+++ app/skeleton/page.tsx
@@ -1,27 +1,8 @@
-import { PokemonCardSkeleton } from "../ui/skeletons"
 
 export default function Skeleton() {
   return (
-    <div className="grid grid-cols-1 gap-8 pt-6
-      xl:grid-cols-2 
-      2xl:grid-cols-4 
-    ">
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
-      <PokemonCardSkeleton />
+    <div>
+
     </div>
   )
 }
