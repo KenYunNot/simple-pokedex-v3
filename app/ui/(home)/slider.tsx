4fbb1e4501a1183a53326868d2983bdc2f4cd509
Ken Yun <yunhaen@gmail.com>
2024-01-19 00:13:37 -0500

    Added links to all slider items

--- app/ui/(home)/slider.tsx
+++ app/ui/(home)/slider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { Fragment, useState } from "react";
 import { PokemonDetailed } from "@/app/lib/definitions";
 import TypeIcon from "@/app/ui/type/type-icon";
@@ -28,11 +29,11 @@ export default function Slider({
   }
 
   const translate = -1 * (highlighted * 256 + 896);  // Calculate the translation in pixels (account for the cloned Pokemon)
-  const arrowStyle = "text-white bg-gray-500 opacity-70 rounded-lg hover:opacity-100"
+  
   return (
     <div className="slider-container">
-      <button className="slider-button left" onClick={moveLeft}><ChevronLeftIcon className={arrowStyle} /></button>
-      <button className="slider-button right" onClick={moveRight}><ChevronRightIcon className={arrowStyle} /></button>
+      <button className="slider-button left" onClick={moveLeft}><ChevronLeftIcon /></button>
+      <button className="slider-button right" onClick={moveRight}><ChevronRightIcon /></button>
       <ul
         className="slider"
         style={{
@@ -86,53 +87,55 @@ function CarouselItem({
   clone?: boolean,
 }) {
   return (
-    <li 
-      className={clsx("border-r-4 border-gray-800", {
-        "w-64 h-96 border-b-4" : !highlighted,
-        "w-96 rounded-md" : highlighted,
-        "clone" : clone,
-      })} 
-      key={pokemon.id}
-    >
-      <div className="relative">
-        <p className="absolute w-full h-64 pt-10 text-8xl text-gray-600 text-center font-semibold opacity-30">
-          {pokemon.id}
-        </p>
-        <Image
-          className="bg-gray-300 z-0"
-          src={pokemon.image_url}
-          width={475}
-          height={475}
-          alt={`Image of ${pokemon.form_name}`}
-        />
-      </div>
-      <div
-        className={clsx("px-4 py-6 bg-gray-700", {
-          "h-44 rounded-bl-md": highlighted,
-          "h-32" : !highlighted,
-        })}
+    <Link href={`/pokedex/${pokemon.name}`}>
+      <li 
+        className={clsx("border-r-4 border-gray-800 slider-item hover:opacity-90", {
+          "w-64 h-96 border-b-4" : !highlighted,
+          "w-96 rounded-br-md" : highlighted,
+          "clone" : clone,
+        })} 
+        key={pokemon.id}
       >
-        <p className="text-2xl text-white font-semibold">
-          {pokemon.species.full_name}
-        </p>
-        {highlighted && (
-          <dl className="mt-3">
-            <dt className="w-24 mb-3 float-left text-lg text-white font-semibold">Type</dt>
-            <dd className="flex mb-3">
-              {pokemon.types.map((type) => {
-                return (
-                  <Fragment key={type.id}>
-                    <TypeIcon className="mx-0.5" type={type} link={true} />
-                  </Fragment>
-                );
-              })}
-            </dd>
+        <div className="relative">
+          <p className="absolute w-full h-64 pt-10 text-8xl text-gray-600 text-center font-semibold opacity-30">
+            {pokemon.id}
+          </p>
+          <Image
+            className="bg-gray-300 z-0"
+            src={pokemon.image_url}
+            width={475}
+            height={475}
+            alt={`Image of ${pokemon.form_name}`}
+          />
+        </div>
+        <div
+          className={clsx("px-4 py-6 bg-gray-700", {
+            "h-44 rounded-bl-md": highlighted,
+            "h-32" : !highlighted,
+          })}
+        >
+          <p className="text-2xl text-white font-semibold">
+            {pokemon.species.full_name}
+          </p>
+          {highlighted && (
+            <dl className="mt-3">
+              <dt className="w-24 mb-3 float-left text-lg text-white font-semibold">Type</dt>
+              <dd className="flex mb-3">
+                {pokemon.types.map((type) => {
+                  return (
+                    <Fragment key={type.id}>
+                      <TypeIcon className="mx-0.5" type={type} link={true} />
+                    </Fragment>
+                  );
+                })}
+              </dd>
 
-            <dt className="w-24 mb-3 float-left text-lg text-white font-semibold">Genus</dt>
-            <dd className="mb-3 text-lg text-white">{pokemon.species.genus}</dd>
-          </dl>
-        )}
-      </div>
-    </li>
+              <dt className="w-24 mb-3 float-left text-lg text-white font-semibold">Genus</dt>
+              <dd className="mb-3 text-lg text-white">{pokemon.species.genus}</dd>
+            </dl>
+          )}
+        </div>
+      </li>
+    </Link>
   );
 }
