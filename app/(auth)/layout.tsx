5c2039ee944963569ed544d4881ce2e8c1bebf95
Ken Yun <yunhaen@gmail.com>
2024-02-13 16:17:39 -0500

    Moved auth routes to separate folder

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+
+export default function AuthLayout({
+  children
+} : {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full bg-red-500">
+      {children}
+    </div>
+  )
+}
