f4c9cdec59350d97fcd13a6244a380e81d18a0af
Ken Yun <yunhaen@gmail.com>
2024-02-25 16:41:16 -0500

    Added vertical margins for xl screen sizes (1280px width)

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <main className='min-h-screen mx-[auto] px-3 py-8 max-w-7xl bg-white rounded'>
+        <main className='min-h-screen mx-[auto] xl:my-3 px-3 py-8 max-w-7xl bg-white rounded'>
           {children}
         </main>
       </body>
