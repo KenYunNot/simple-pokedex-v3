0baac0d5eb4bc0d39086662afb4476223c227903
Ken Yun <yunhaen@gmail.com>
2024-03-04 13:16:09 -0500

    fetchPokemon function returns list of new CardData type

--- lib/data/pokemon.ts
+++ lib/data/pokemon.ts
@@ -1,5 +1,5 @@
+
 import prisma from "@/lib/prisma";
-import { cache } from "react";
 
 
 const ITEMS_PER_PAGE = 16;
@@ -10,7 +10,7 @@ export async function fetchPokemon(
 ) {
   const offset = ITEMS_PER_PAGE * (page - 1);
   try {
-    let pokemon = await prisma.pokemon.findMany({
+    const data = await prisma.pokemon.findMany({
       where: {
         name: {
           contains: query,
@@ -23,6 +23,15 @@ export async function fetchPokemon(
       },
       skip: offset,
       take: ITEMS_PER_PAGE,
+    });
+    const pokemon = data.map(p => {
+      return {
+        id: p.id,
+        name: p.name,
+        full_name: p.species.full_name,
+        image_url: p.image_url,
+        types: p.types,
+      }
     });
     return pokemon;
   } catch (error) {
@@ -60,11 +69,43 @@ export async function fetchPokemonById(id: number) {
   }
 }
 
+
+/* Given an id number, fetch an individual Pokemon */
+export async function fetchPokemonByName(name: string) {
+  try {
+    let pokemon = await prisma.pokemon.findFirst({
+      where: {
+        name,
+      },
+      include: {
+        species: true,
+        types: {
+          include: {
+            double_damage_from: true,
+            double_damage_to: true,
+            half_damage_from: true,
+            half_damage_to: true,
+            no_damage_from: true,
+            no_damage_to: true,
+            pokemon: true,
+          }
+        }
+      }
+    });
+    return pokemon;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error(`Failed to fetch Pokemon with name ${name}`);
+  }
+}
+
+
+
 /**
  * Fetch a number of random Pokemon
  * @returns 
  */
-async function frp(count=10) {
+export async function fetchRandomPokemon(count=10) {
   try {
     let aggregations = await prisma.pokemon.aggregate({
       _count: {
@@ -100,33 +141,8 @@ async function frp(count=10) {
     throw new Error(`Failed to fetch Pokemon`);
   }
 }
-export const fetchRandomPokemon = cache(frp);
 
 
-/**
- * Given an identifier either in the form of a number id or a string name, return the corresponding Pokemon's id
- * @param identifier 
- */
-export async function findPokemonId(identifier: string) : Promise<number> {
-  let id = Number(identifier);
-  // If the identifier is already a number, return it as the id
-  if (!Number.isNaN(id)) 
-    return id;
-  try {
-    let pokemon = await prisma.pokemonSpecies.findFirst({
-      where: {
-        name: identifier,
-      },
-    }); 
-    // If no Pokemon was found, return an invalid id
-    if (!pokemon) 
-      return -1;
-    return pokemon.id;
-  } catch (error) {
-    console.error("Database Error:", error);
-    throw new Error(`Failed to fetch Pokemon ID for \'${identifier}\'`);
-  }
-}
 
 /* Counts the total number of pages (16 Pokemon per page) for a given query */
 export async function countPokemonPages(query: string) {
