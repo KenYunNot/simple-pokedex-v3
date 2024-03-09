4fd4e7e4b823744014928d515e9148268a94f63c
Ken Yun <yunhaen@gmail.com>
2024-03-09 12:23:23 -0500

    Added rounded borders and hidden overflow to bento components

--- lib/ui/bento-component.tsx
+++ lib/ui/bento-component.tsx
@@ -6,14 +6,14 @@ export default function BentoComponent({
 }: {
   rowSpan: number;
   colSpan: number;
-  className: string;
+  className?: string;
   children?: React.ReactNode;
 }) {
   /* col-span-1  col-span-2  col-span-3  col-span-4  col-span-5 */
   /* row-span-1  row-span-2  row-span-3  row-span-4  row-span-5 */
 
   return (
-    <div className={`group/component col-span-${colSpan} row-span-${rowSpan} ${className}`}>
+    <div className={`group/component col-span-${colSpan} row-span-${rowSpan} rounded-lg overflow-hidden ${className}`}>
       {!!children ? children : "Some filler text"}
     </div>
   );
