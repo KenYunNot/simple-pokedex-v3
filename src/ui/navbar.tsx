143e48dad6eeff58c9545d4c6b06b1508daccef2
Ken Yun <yunhaen@gmail.com>
2024-10-14 12:04:55 -0400

    fix: added basic styles to desktop navbar

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -13,8 +13,8 @@ const links = [
 
 const Navbar = () => {
   return (
-    <nav className='relative flex justify-end items-center w-full h-12 px-2 bg-pokeballred text-white'>
-      <p className='absolute left-2  font-bold'>Simple Pokedex v3</p>
+    <nav className='relative flex items-center h-12 md:h-20 px-2 bg-pokeballred text-white'>
+      <p className='absolute left-2 font-bold'>Simple Pokedex v3</p>
       <Mobile />
       <TabletDesktop />
     </nav>
@@ -25,9 +25,9 @@ const Mobile = () => {
   const [menuToggle, setMenuToggle] = React.useState(false);
 
   return (
-    <div>
+    <div className='flex md:hidden justify-end w-full'>
       <MenuIcon className='p-1 w-8 h-8 rounded-sm' onClick={() => setMenuToggle(!menuToggle)} />
-      <ul className={cn('absolute top-12 left-0 flex flex-col w-full bg-white text-pokeballred border border-t-0 border-pokeballred list-none z-50 md:hidden', {
+      <ul className={cn('absolute top-12 left-0 flex flex-col w-full bg-white text-pokeballred border border-t-0 border-pokeballred list-none z-50', {
         'hidden' : menuToggle === false,
       })}>
         {links.map(link => (
@@ -44,8 +44,16 @@ const Mobile = () => {
 
 const TabletDesktop = () => {
   return (
-    <div className='hidden md:block'>
-      TabletDesktop
+    <div className='hidden md:flex justify-center w-full'>
+      <ul className='flex gap-3 mx-auto text-white list-none'>
+        {links.map(link => (
+          <li className='flex w-32 h-12 font-bold rounded-md duration-200 hover:bg-white hover:text-pokeballred'>
+            <Link key={link.href} href={link.href} className='flex justify-center items-center w-full h-full'>
+              {link.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
